2054788d7fee8ed4d13d2b134f1c36726588ed08
Ayana Osawa <ayana.osawa@gmail.com>
2020-10-29 10:43:46 -0400

    Add clean plugin, update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,18 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+	mode: 'development', // Minify js
 	watch: true,
 	entry: './src/js/app.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-						// options: { minimize: true }
-					},
-				],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							esModule: false,
-						},
-					},
-				],
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-		],
-	},
 	plugins: [
+		new CleanWebpackPlugin(),
 		new HtmlWebPackPlugin({
 			template: './src/index.html',
 			filename: 'index.html',
@@ -58,4 +26,40 @@ module.exports = {
 			chunkFilename: '[id].css',
 		}),
 	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader', // 3. Inject styles into DOM
+					'css-loader', // 2. Turns css into commonjs
+					'sass-loader', // 1. Turns sass into css
+				],
+			},
+			{
+				test: /\.html$/i,
+				use: ['html-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name]_[hash].[ext]',
+							outputPath: 'images',
+							esModule: false,
+						},
+					},
+				],
+			},
+		],
+	},
 }
